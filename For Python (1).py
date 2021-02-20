23d35e33d9e6972ce34b51c946083de0a3aadb36
AliAshraf7868 <aliashraf7868@gmail.com>
2021-02-20 08:59:54 +0500

    First python program

---
+++ For Python (1).py
@@ -0,0 +1,28 @@
+Movie="Murder2"
+Song_Tittle="Aye Khuda"
+date=30
+month=8
+year=2018
+Genre="Emotional song"
+mint=7
+sec=8
+Artist="Kshitij Tarey, Saim Bhat And Mithoon"
+Album="Murder 2"
+Licensed="TSeries"
+Views=5856305
+likes=71000
+Dislike=7.6
+def About_Song():
+    print("My favorite song is :",Song_Tittle)
+    print("The Album of this sond is :",Album)
+    print("It Released on :",date,"/",month,"/",year)
+    print("The Genre of this song is :",Genre)
+    print("The duration of this donge is :",mint,"min",sec,"sec")
+    print("The Artists of this song is :",Artist)
+    print("The views of this song on YouTube is :",Views)
+    print("The like on YouTube is :",likes)
+    print("The dislike on YouTube is :",Dislike,"k")
+    return
+
+
+About_Song()
